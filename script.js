68ae83a00430e15a8f1ac0810871d157b746729f
nickynite <nbeullac@gmail.com>
2021-08-12 20:03:36 -0500

    added text

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-console.log('hello');
 
-function toggleTheme() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementsByTagName('link')[0];
+// function toggleTheme() {
 
-    // Change the value of href attribute 
-    // to change the css sheet.
-    if (theme.getAttribute('href') == './styles/styles.css') {
-        theme.setAttribute('href', './styles/stylesdark.css');
-    } else {
-        theme.setAttribute('href', './styles/styles.css');
-    }
-}
+//     // Obtains an array of all <link>
+//     // elements.
+//     // Select your element using indexing.
+//     var theme = document.getElementsByTagName('link')[0];
+
+//     // Change the value of href attribute 
+//     // to change the css sheet.
+//     if (theme.getAttribute('href') == './styles/styles.css') {
+//         theme.setAttribute('href', './styles/stylesdark.css');
+//     } else {
+//         theme.setAttribute('href', './styles/styles.css');
+//     }
+// }
+
+
+// Comparing this with one above
+document.addEventListener('DOMContentLoaded', () => {
+
+    const themeStylesheet = document.getElementById('theme');
+    const themeToggle = document.getElementById('theme-toggle');
+    themeToggle.addEventListener('click', () => {
+        if(themeStylesheet.href.includes('styles.css')) {
+            themeStylesheet.href = './styles/stylesdark.css';
+        } else {
+            themeStylesheet.href = './styles/styles.css';
+        }
+    })
+})
+
